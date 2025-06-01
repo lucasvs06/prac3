cfdd5c30d423a8dd9c840f7247ce3051fde51a8d
DennisThomassen <thomvanmaren@gmail.com>
2025-06-01 18:37:32 +0200

    base file shit

---
+++ app/Http/Controllers/TeamController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\team;
+
+class TeamController extends Controller
+{
+    public function index(){
+        $teams = Team::all();
+        return view('teams.index')->compact('team');
+    }
+}
--- database/migrations/2025_06_01_152538_create_teams.php
+++ database/migrations/2025_06_01_152538_create_teams.php
@@ -11,8 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->enum('role', ['player', 'moderator', 'gambler', 'gamblingModerator', 'admin'])->default('player')->after('email');
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('player_count');
+            $table->string('coach_name');
+            $table->timestamps();
         });
     }
 
@@ -21,8 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('role');
-        });
+        Schema::dropIfExists('teams');
     }
 };
